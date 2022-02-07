cfe5127ce65fe46c7c2cae11248de160f3a4936e
agakrowinska <aga.krowinska@gmail.com>
2022-02-07 13:37:03 +0100

    security done for now

--- src/main/java/com/example/demo/security/PasswordEncoder.java
+++ src/main/java/com/example/demo/security/PasswordEncoder.java
@@ -1,4 +1,19 @@
 package com.example.demo.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+//not storing the password in plaintext
+
+@Configuration
 public class PasswordEncoder {
+
+
+    //each call will have a different result
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/com/example/demo/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/security/config/WebSecurityConfig.java
@@ -1,13 +1,60 @@
 package com.example.demo.security.config;
 
+import com.example.demo.appDoctor.AppDoctorService;
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @AllArgsConstructor
 @EnableWebSecurity
+//enable Spring Security’s web security support and provide the Spring MVC integration
+
 
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AppDoctorService appDoctorService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                //I can send post requests without being rejected
+                //crsf  = Cross-Site Request Forgery attack
+                .authorizeRequests()
+                    .antMatchers("/api/v*/registration/**")
+                // this HttpSecurity will only be applicable to URLs that start with the path
+                    .permitAll()
+                //i want to allow everything under registration
+                .anyRequest()
+                .authenticated().and()
+                .formLogin();
+        //we want to require all users to be authenticated
+
+    }
+
+
+    //defines which URL paths should be secured and which should not.
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    //retrieves the user details from a simple, read-only user DAO – the UserDetailsService.
+    // User Details Service only has access to the username in order to retrieve
+    // the full user entity.
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setPasswordEncoder(bCryptPasswordEncoder);
+        authProvider.setUserDetailsService(appDoctorService);
+        return authProvider;
+    }
 }
