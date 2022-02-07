3b1a83b1dd2b4a9267f523af39f0cb7af641ae1c
agakrowinska <aga.krowinska@gmail.com>
2022-02-07 14:12:29 +0100

    trying the advanced rest to work

--- src/main/java/com/example/demo/registration/RegistrationController.java
+++ src/main/java/com/example/demo/registration/RegistrationController.java
@@ -1,6 +1,7 @@
 package com.example.demo.registration;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,8 @@ public class RegistrationController {
     //reference to the service
     private RegistrationService registrationService;
 
+    //annotation postmapping so our adve=ander Rest client can see it
+    @PostMapping
     public String register(@RequestBody RegistrationRequest request){
         return registrationService.register(request);
     }
