1fcf067a00f1dc8e4ffc9d8f1bc30fc5f749b1b5
agakrowinska <aga.krowinska@gmail.com>
2022-02-08 12:00:37 +0100

    email sending

--- src/main/java/com/example/demo/email/EmailService.java
+++ src/main/java/com/example/demo/email/EmailService.java
@@ -15,6 +15,8 @@ import javax.mail.internet.MimeMessage;
 @AllArgsConstructor
 public class EmailService implements EmailSender{
 
+
+        //Default configurations
     private final static Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
 
     private final JavaMailSender mailSender;
@@ -26,16 +28,15 @@ public class EmailService implements EmailSender{
     public void send(String to, String email) {
         try{
         MimeMessage mimeMessage = mailSender.createMimeMessage();
-        MimeMessageHelper helper =
-                new MimeMessageHelper(mimeMessage, "utf-8");
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
         helper.setText(email, true);
         helper.setTo(to);
-        helper.setSubject("Confirm your email");
-        helper.setFrom("hello@amigoscode.com");
+        helper.setSubject("Please confirm your email");
+        helper.setFrom("a.h.krowinska@gmail.com");
         mailSender.send(mimeMessage);
     } catch (MessagingException e) {
-        LOGGER.error("failed to send email", e);
-        throw new IllegalStateException("failed to send email");
+        LOGGER.error("Failed to send Email", e);
+        throw new IllegalStateException("Failed to send Email");
     }
     }
 }
